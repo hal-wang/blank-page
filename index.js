c1f24ae32428f1e0387996e84372688c4ca5c148
hal-wang <hi@hal.wang>
2021-05-10 23:13:23 +0800

    fix: img err

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const Funceb = require("@hal-wang/funceb").default;
 const fs = require("fs");
 
 exports.main = async (event) => {
-  const funceb = new Funceb(event.path, "web");
+  const funceb = new Funceb(event.path, "web", "base64");
 
   return {
     isBase64Encoded: true,
     statusCode: funceb.status,
     headers: { "Content-Type": funceb.mine },
-    body: fs.readFileSync(funceb.filePath, "base64"),
+    body: funceb.content,
   };
 };
